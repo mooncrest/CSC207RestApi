0d44783304dd648523fda3aafea9b4cf4bd54e9b
mooncrest <pengray4@gmail.com>
2019-11-20 22:25:26 -0500

    Removed uneeded model ScorePostToken

--- src/main/java/CSC207/CSC207RestApi/api/Tokens/ScorePoster.java
+++ src/main/java/CSC207/CSC207RestApi/api/Tokens/ScorePoster.java
@@ -1,11 +1,14 @@
 package CSC207.CSC207RestApi.api.Tokens;
 
 import CSC207.CSC207RestApi.model.Score;
-import CSC207.CSC207RestApi.model.ScorePostToken;
 import CSC207.CSC207RestApi.model.Token;
 import CSC207.CSC207RestApi.service.Tokens.TokensService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
 
 @RequestMapping("api/tokens/score")
 @RestController
@@ -19,10 +22,15 @@ public class ScorePoster {
     }
 
     @PutMapping(path = "{game}")
-    public void addScore(@PathVariable("game") String game, @RequestBody ScorePostToken token) {
-        Token userToken = token.getToken();
-        Score userScore = token.getScore();
-        tokensService.insertScore(userToken, userScore, game);
+    public void addScore(@PathVariable("game") String game, @RequestBody ObjectNode json) {
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            Token token = mapper.readValue(json.get("token").toString(), Token.class);
+            Score score = mapper.readValue(json.get("score").toString(), Score.class);
+            tokensService.insertScore(token, score, game);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
 
